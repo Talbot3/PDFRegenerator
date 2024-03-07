5fd240126140226b2d99ec4c03998f8ca0566164
Talbot3 <4wunian@gmail.com>
2024-03-07 20:43:35 +0800

    Auto-generated summary:   Last commit: c281b3c - Talbot3, 77 秒钟前 : Auto-generated summary: Last commit: f16de7b - Talbot3, 10 分钟前 : Auto-generated summary: Last commit: 5e76b88 - Talbot3, 14 分钟前 : Auto-generated summary: Last commit: fefd1bc - Talbot3, 14 秒钟前 : Auto-generated summary: Last commit:   File changes:

--- auto_commit.sh
+++ auto_commit.sh
@@ -8,10 +8,9 @@ file_changes=$(git diff --name-status)
 
 # 生成 commit 消息
 commit_message="Auto-generated summary:
-Last commit: $last_commit_info
-
-File changes:
-$file_changes"
+  Last commit: $last_commit_info
+  File changes:
+    $file_changes"
 
 # 提交到 Git
 git commit -am "$commit_message"
