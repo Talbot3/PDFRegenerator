f096c84ee46755b46ca05b2394589aa4cb5435c7
Talbot3 <4wunian@gmail.com>
2024-03-07 20:45:03 +0800

    Auto-generated summary:   Last commit: Thu Mar  7 20:45:02 CST 2024   File changes:

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 获取最后一次提交的信息
-last_commit_info=$(git log -1 --pretty=format:"%h - %an, %ar : %s")
+last_commit_date=$(date)
 
 # 获取最近的文件变动
 file_changes=$(git diff --name-status)
 
 # 生成 commit 消息
 commit_message="Auto-generated summary:
-  Last commit: $last_commit_info
+  Last commit: $last_commit_date
   File changes:
     $file_changes"
 
