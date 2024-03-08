53317e7999e11521601fd42b6ab9f686f0cbcaa7
Talbot3 <4wunian@gmail.com>
2024-03-08 18:08:26 +0800

    Auto-generated summary:   Commit Date: Fri Mar  8 18:08:25 CST 2024   File changes:

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-
+# Regenerator PDF
 Regenerator PDF 是一款开源工具，可帮助您将PDF文档 转换为有辅助信息的PDF。它可以执行以下功能：
 - **标记到索引单元**：将 PDF 文档中的标记转换为索引单元，以便于快速导航和搜索。
 Regenerator PDF 是开源且免费使用的，使其成为学生、研究人员和专业人士的宝贵工具。它兼容 Windows、Mac 和 Linux 操作系统。 
 
+## 安装
+
+```
+pip install pdf-regenerator
+```
+
+## 生成PDF目录
+
+```
+python3 -m pdf-regenerator $YOU_PDF_PATH $YOU_OUT_FILE_DIR
+```
--- pdf_regenerator/__main__.py
+++ pdf_regenerator/__main__.py
@@ -1,3 +1,5 @@
+import os
+
 import  fitz  # PyMuPDF
 from reportlab.pdfgen import canvas
 from PyPDF2 import PdfReader, PdfWriter
@@ -6,7 +8,8 @@ import click
 
 @click.command()
 @click.argument('pdf_path', type=click.Path(exists=True))
-def create_pdf_with_contents(pdf_path):
+@click.option('--output_path', '-o', help='Output file path', type=click.Path(), default=lambda: os.path.join(os.getcwd()))
+def create_pdf_with_contents(pdf_path, output_path):
     doc = fitz.open(pdf_path)
     toc = []
     titles = []
@@ -35,8 +38,13 @@ def create_pdf_with_contents(pdf_path):
     for i in range(len(existing_pdf.pages)):
         output.add_page(existing_pdf.pages[i]) # 更新调用方式
 
-    output_path = "output_with_contents.pdf"
-    with open(output_path, "wb") as outputStream:
+    file_name, file_extension = os.path.splitext(os.path.basename(pdf_path))
+    # 添加标记到文件名
+    if output_path:
+        mark = 'mark'
+        output_file_path = os.path.join(os.path.dirname(output_path), f'{file_name}_{mark}{file_extension}')
+
+    with open(output_file_path, "wb") as outputStream:
         output.write(outputStream)
 if __name__ == "__main__":
     create_pdf_with_contents()
